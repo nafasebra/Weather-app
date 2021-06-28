e82e008ce1666bc21d1af702c8bd6f0edd1b70ca
Nafiseh Ebrahimi <ebranafas@gmail.com>
2021-06-28 12:25:07 +0430

    update weatherapp-reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
+const api = {
+  key: "aca9ec2a5fd98688c095263751e23330",
+  baseLink: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2 className="status_text">Sunny</h2>
+      <h4 className="temp_text">50^c</h4>
+      <p className="location_text">Kashan city, iran</p>
+      <p className="date_text">sunday, june 21, 2021</p>
     </div>
   );
 }
